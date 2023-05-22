5cccd9dac455eef6daada72fc9754edaa50f60e5
Youngbin Han <youngbin.han@cloudmt.co.kr>
2023-05-22 16:50:04 +0900

    Credential 조회 API 테스트 추가

--- RPM.Api.Tests/Controllers/CredentialControlerTests.cs
+++ RPM.Api.Tests/Controllers/CredentialControlerTests.cs
@@ -14,7 +14,8 @@ public class CredentialControlerTests
     public void GetById()
     {
         var mockQueries = new Mock<ICredentialQueries>();
-        var mockData = new Credential(){
+        var mockData = new Credential()
+        {
             CredId = 1,
             AccountId = 1,
             Vendor = "VEN-XXX",
@@ -31,11 +32,57 @@ public class CredentialControlerTests
             Mock.Of<ICredentialRepository>(),
             Mock.Of<IMapper>()
         );
-        
+
         var result = controller.GetById(1, 1);
+        Assert.IsType<Credential>(result);
+        Assert.Equal("VEN-XXX", result.Vendor);
+        Assert.Equal("test", result.CredName);
+    }
 
-        Assert.Equal("VEN-XXX", result.Vendor); 
-        
+    [Fact]
+    public void GetList()
+    {
+        var mockQueries = new Mock<ICredentialQueries>();
+        var mockData = new List<Credential>()
+        {
+            new Credential()
+            {
+                CredId = 1,
+                AccountId = 1,
+                Vendor = "VEN-XXX",
+                CredName = "test",
+                IsEnabled = true
+            },
+            new Credential()
+            {
+                CredId = 2,
+                AccountId = 1,
+                Vendor = "VEN-ABC",
+                CredName = "ABC",
+                IsEnabled = false
+            }
+        };
+        mockQueries
+            .Setup(
+                x =>
+                    x.GetCredentials(
+                        It.IsAny<long>(),
+                        It.IsAny<string>(),
+                        It.IsAny<string>(),
+                        It.IsAny<bool>()
+                    )
+            )
+            .Returns(mockData);
 
+        var controller = new CredentialController(
+            null,
+            mockQueries.Object,
+            Mock.Of<ICredentialRepository>(),
+            Mock.Of<IMapper>()
+        );
+
+        var result = controller.GetList(1, "", "", true);
+        Assert.IsType<List<Credential>>(result);
+        Assert.Equal(2, result.Count());
     }
 }
