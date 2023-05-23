7e822a5ba3c52ffce26ebed7fea1c74926e95c86
Youngbin Han <youngbin.han@cloudmt.co.kr>
2023-05-23 15:39:54 +0900

    WIP: POST Credential API 테스트 작성

--- RPM.Api.Tests/Controllers/CredentialControlerTests.cs
+++ RPM.Api.Tests/Controllers/CredentialControlerTests.cs
@@ -1,8 +1,14 @@
 using RPM.Domain.Models;
+using RPM.Domain.Commands;
 using RPM.Domain.Mappers;
+using RPM.Domain.Dto;
 using RPM.Api.Controllers;
 using RPM.Api.App.Repository;
 using RPM.Api.App.Queries;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Security.Principal;
+using System.Security.Claims;
 using Moq;
 using AutoMapper;
 
@@ -84,5 +90,43 @@ public class CredentialControlerTests
         var result = controller.GetList(1, "", "", true);
         Assert.Equal(true, typeof(IEnumerable<Credential>).IsAssignableFrom(result.GetType()));
         Assert.Equal(2, result.Count());
+    }
+
+    [Fact]
+    public void AddCredential()
+    {
+        var mockRepo = new Mock<ICredentialRepository>();
+        var mockInput = new CredentialModifyDto();
+        var mockData = new Credential();
+
+        mockRepo
+            .Setup(x => x.CreateSingleCredential(It.IsAny<CredentialModifyCommand>()))
+            .Returns(mockData)
+            .Verifiable();
+
+        var user = new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, "SomeValueHere"),
+                    new Claim(ClaimTypes.Name, "a@b.c")
+                    // other required and custom claims
+                },
+                "TestAuthentication"
+            )
+        );
+
+        var controller = new CredentialController(
+            null,
+            Mock.Of<ICredentialQueries>(),
+            mockRepo.Object,
+            Mock.Of<IMapper>()
+        );
+        controller.ControllerContext = new ControllerContext();
+        controller.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
+        
+        var result = controller.AddCredential(1, mockInput);
+        Assert.IsType<Credential>(result);
+        mockRepo.Verify();
     }
 }
--- RPM.Domain/Dto/CredentialModifyDto.cs
+++ RPM.Domain/Dto/CredentialModifyDto.cs
@@ -1,10 +1,10 @@
 namespace RPM.Domain.Dto;
 
-    public class CredentialModifyDto
-    {
-        public string Vendor { get; set; }
-        public string CredName { get; set; }
-        public bool IsEnabled { get; set; }
-        public string CredData { get; set; }
-        public string Note { get; set; }
-    }
+public class CredentialModifyDto
+{
+    public string Vendor { get; set; }
+    public string CredName { get; set; }
+    public bool IsEnabled { get; set; }
+    public string CredData { get; set; }
+    public string Note { get; set; }
+}
