4ab7e577beb74ae07a6e3ab5fb6223471d6256a7
Youngbin Han <youngbin.han@cloudmt.co.kr>
2023-05-31 05:28:27 +0000

    Updated Program.cs

--- RPM.Api/Program.cs
+++ RPM.Api/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials();
     });
 });
 
