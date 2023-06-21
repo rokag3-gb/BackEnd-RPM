73d8987db7867991e70375cbeb725d05a2aa4b93
Youngbin Han <youngbin.han@cloudmt.co.kr>
2023-06-21 11:52:55 +0900

    Credential 조회 isEnable 기본값 null 지정

--- RPM.Api/Controllers/CredentialController.cs
+++ RPM.Api/Controllers/CredentialController.cs
@@ -47,7 +47,7 @@ public class CredentialController : ControllerBase
         [SwaggerParameter("대상 조직 ID", Required = true)] long accountId,
         [SwaggerParameter("클라우드 벤더 코드(VEN-XXX 형식)", Required = false)] string? vendor,
         [SwaggerParameter("자격증명 이름 검색 키워드", Required = false)] string? credName,
-        [SwaggerParameter("자격증명 사용 여부", Required = false)] bool? isEnabled = true
+        [SwaggerParameter("자격증명 사용 여부", Required = false)] bool? isEnabled = null
     )
     {
         var credentials = _credentialQueries.GetCredentials(accountId, vendor, credName, isEnabled);
