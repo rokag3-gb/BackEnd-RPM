d23e5d2fc3ef8e6dd1bc28d429058d0f94bfba80
Youngbin Han <youngbin.han@cloudmt.co.kr>
2023-06-27 14:10:47 +0900

    WIP P2 관련 로직 초안

---
+++ RPM.Api/App/Commands/RegisterInstanceJobCommand.cs
@@ -0,0 +1,10 @@
+using MediatR;
+
+namespace RPM.Api.App.Commands;
+
+public class RegisterInstanceJobCommand : IRequest<int>
+{
+    public long CredId { get; set; }
+    public long AccountId { get; set; }
+
+}
--- RPM.Infra/Clients/P2Client.cs
+++ RPM.Infra/Clients/P2Client.cs
@@ -10,10 +10,20 @@ public class P2Client {
     }
 
     public void HelloWorld() {
-        var client = new  ScheduleCreateApiService.ScheduleCreateApiServiceClient(_grpcChannel);
-        client.CreateSchedules(new CreateSchedulesRequest(){
+        var client = new ScheduleCreateApiService.ScheduleCreateApiServiceClient(_grpcChannel);
+        var request = new CreateSchedulesRequest(){
             JobId = 1
-            
+        };
+        request.CreateSchedules.Add(new CreateScheduleData(){
+            AccountId = 1,
+            ScheduleName = "test",
+            Cron = "*/5 * * * * *",
+            IsEnable = true,
+            ActivateDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+            ExpireDate = DateTime.Now.AddDays(1).ToString("yyyy-MM-dd HH:mm:ss"),
+            Note = "test",
+            SaveUserId = "sss",
         });
+        client.CreateSchedules(request);
     }
 }
