1b4bed45ca869b93386a12f4837cccdfa5985ae9
Youngbin Han <youngbin.han@cloudmt.co.kr>
2023-07-13 14:34:23 +0900

    Yaml 파일 로드 추가

--- RPM.Api/App/Commands/RegisterInstanceJobCommandHandler.cs
+++ RPM.Api/App/Commands/RegisterInstanceJobCommandHandler.cs
@@ -9,18 +9,61 @@ public class RegisterInstanceJobCommandHandler : IRequestHandler<RegisterInstanc
     ICredentialQueries _credentialQueries;
     IInstanceQueries _instanceQueries;
     IP2Client _p2Client;
+    private readonly IConfiguration _config;
+
     public RegisterInstanceJobCommandHandler(
         ICredentialQueries credentialQueries,
         IInstanceQueries instanceQueries,
-        IP2Client p2Client
-    ){
+        IP2Client p2Client,
+        IConfiguration config
+    )
+    {
         _credentialQueries = credentialQueries;
         _instanceQueries = instanceQueries;
         _p2Client = p2Client;
+        _config = config;
     }
-    public async Task<int> Handle(RegisterInstanceJobCommand request, CancellationToken cancellationToken)
+
+    public async Task<int> Handle(
+        RegisterInstanceJobCommand request,
+        CancellationToken cancellationToken
+    )
     {
-        var newJobId = await _p2Client.RegisterJobYaml(request.AccountId, "", request.Note, request.SavedByUserId);
+        // VM, Credential 목록 쿼리
+        // VM 목록, Credential Dict 만들기
+        // YAML 로딩하여 VM 목록, Credential Dict 데이터 삽입
+        var yamlWorkflowFilePath = _config.GetConnectionString("YamlWorkflowFilePath");
+        if (string.IsNullOrEmpty(yamlWorkflowFilePath)){
+            return -1;
+        }
+        try
+        {
+            // Open the file using File.Open
+            using (FileStream fileStream = File.Open(yamlWorkflowFilePath, FileMode.Open))
+            {
+                // Perform operations on the opened file
+                // For example, you can read the file content using a StreamReader
+                using (StreamReader streamReader = new StreamReader(fileStream))
+                {
+                    string fileContent = streamReader.ReadToEnd();
+                    var newJobId = await _p2Client.RegisterJobYaml(
+                        request.AccountId,
+                        fileContent,
+                        request.Note,
+                        request.SavedByUserId
+                    );
+                    return 0;
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found.");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("An error occurred while opening the file.");
+        }
         return 0;
     }
 }
