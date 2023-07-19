996039a73ce0b1d5ed8fc8db515b124b63860e24
Youngbin Han <youngbin.han@cloudmt.co.kr>
2023-07-19 10:38:54 +0900

    JobId 필드 삭제

--- RPM.Domain/Dto/InstancesJobRegisterDto.cs
+++ RPM.Domain/Dto/InstancesJobRegisterDto.cs
@@ -3,7 +3,7 @@ namespace RPM.Domain.Dto;
 public class InstancesJobRegisterDto
 {
     public IEnumerable<long> InstIds { get; set; }
-    public long JobId { get; set; }
+    // public long JobId { get; set; }
     public string ActionCode { get; set; }
     public DateTime SavedAt { get; set; }
 }
