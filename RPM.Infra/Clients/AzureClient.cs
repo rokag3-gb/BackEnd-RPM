4af33fd85d6afc62170609e602f5b61e6f59c1c9
Youngbin Han <youngbin.han@cloudmt.co.kr>
2023-08-07 20:25:30 +0900

    WIP GetAzureVMStatus

--- RPM.Infra/Clients/AzureClient.cs
+++ RPM.Infra/Clients/AzureClient.cs
@@ -28,9 +28,28 @@ public class AzureClient
             {
                 vmList.Add(vm);
             }
-            
         }
         // var vmCollection = subscription.GetVirtualMachinesAsync();
         return vmList;
+    }
+
+    public async Task<string> GetAzureVMStatus(string regionCode, string instanceId)
+    {
+        // Create an instance of the ComputeManagementClient using your Azure credentials
+        ArmClient armClient = new ArmClient(_credential);
+        
+
+        // Get the VM by resource group name and VM name
+        SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+// first we need to get the resource group
+        string rgName = "myRgName";
+        ResourceGroupResource resourceGroup = await subscription.GetResourceGroups().GetAsync(rgName);
+        // Now we get the virtual machine collection from the resource group
+        VirtualMachineCollection vmCollection = resourceGroup.GetVirtualMachines();
+        string vmName = "myVM";
+        VirtualMachineResource vm = await vmCollection.GetAsync(vmName);
+        var vmState = vm.Data.InstanceView.Statuses.Any(s => s.Code == "PowerState/running" || s.Code == "PowerState/starting");
+        return "";
+        // ... print other properties you want to retrieve
     }
 }
